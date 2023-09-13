389faa87780faaa922714d342d77c581cded4511
Giseldo Neo <giseldo@gmail.com>
2023-09-13 18:14:20 -0300

    ultima versao

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import asyncio
 from session_state import get
 from httpx_oauth.clients.google import GoogleOAuth2
 
-
 async def write_authorization_url(client,
                                   redirect_uri):
     authorization_url = await client.get_authorization_url(
@@ -15,21 +14,19 @@ async def write_authorization_url(client,
     )
     return authorization_url
 
-
 async def write_access_token(client,
                              redirect_uri,
                              code):
     token = await client.get_access_token(code, redirect_uri)
     return token
 
-
 async def get_email(client,
                     token):
     user_id, user_email = await client.get_id_email(token)
     return user_id, user_email
 
-
 def main(user_id, user_email):
+    st.write(f"Teste!!!")
     st.write(f"You're logged in as {user_email}")
 
 
